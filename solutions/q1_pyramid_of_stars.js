7fcd3c3ac48712aeb6b3cc5003bb437f143eff8a
Himani <himani.singla@niit.com>
2018-04-05 17:19:36 +0530

    Initial Boilerplate code

---
+++ solutions/q1_pyramid_of_stars.js
@@ -0,0 +1,19 @@
+/* Write a program to build a `Pyramid of stars` of given height */
+
+const buildPyramid = () => {
+	// Write your code here
+};
+
+/* For example,
+INPUT - buildPyramid(6)
+OUTPUT -
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+* * * * * *
+
+*/
+
+module.exports = buildPyramid;
---
+++ solutions/q5_students_result_card.js
@@ -0,0 +1,7 @@
+// Write a program to display one result card of 100 students
+// with their name and percentage
+// Out of 100 students, 50 has subjects - Grammer and Accounts
+// and other 50 has subjects -  Grammer and Physics
+
+
+// Write your code here
