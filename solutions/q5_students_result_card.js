38f35e197c53368fd6c0e98742d679702daec1c9
Himani <himani.singla@stackroute.in>
2018-04-11 14:42:59 +0530

    Update q5_students_result_card.js

--- solutions/q5_students_result_card.js
+++ solutions/q5_students_result_card.js
@@ -3,5 +3,8 @@
 // Out of 100 students, 50 has subjects - Grammer and Accounts
 // and other 50 has subjects -  Grammer and Physics
 
+// Hint : You need to calculate percentage of 100 students having different set of subjects.
+//        You can assume their scores on their respective subjects.
+
 
 // Write your code here
